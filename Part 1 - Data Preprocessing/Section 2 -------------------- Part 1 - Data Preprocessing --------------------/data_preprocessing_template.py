6115db9c851a222c8101547d46711fb5db4194cd
Abdul Samad <abdulsamad@TetraNoodle.local>
2018-09-26 11:42:28 +0300

    Initial commit

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 17:19:06 2018
+
+@author: abdulsamad
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Data.csv')
