d9a39bb2003122e098f4089b1bf3a75427e852c1
Abdul Samad <abdulsamad@TetraNoodle.local>
2018-09-26 13:37:32 +0300

    Data encoding for python and R

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -7,3 +7,12 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary),
                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                         dataset$Salary)
+
+# Labeling data
+
+dataset$Country = factor(dataset$Country, 
+                         c('France', 'Spain', 'Germany'),
+                         c(1,2,3))
+dataset$Purchased = factor(dataset$Purchased, 
+                           c('No', 'Yes'),
+                           c(0,1))
--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -14,9 +14,25 @@ import pandas as pd
 from sklearn.preprocessing import Imputer
 
 dataset = pd.read_csv('Data.csv')
-input_features = dataset.iloc[:, :-1].values
+features = dataset.iloc[:, :-1].values
 result = dataset.iloc[:,3].values
 
+# missing data
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(input_features[:, 1:3])
-input_features[:, 1:3] = imputer.transform(input_features[:, 1:3])
+imputer = imputer.fit(features[:, 1:3])
+features[:, 1:3] = imputer.transform(features[:, 1:3])
+
+# categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+# Encoding input features
+labelEncoder_input  = LabelEncoder()
+features[:, 0] = labelEncoder_input.fit_transform(features[:, 0]) 
+hotEncoder = OneHotEncoder(categorical_features = [0])
+features = hotEncoder.fit_transform(features).toarray()
+
+# Encoding output features
+labelEncoder_output = LabelEncoder()
+result = labelEncoder_output.fit_transform(result)
+
+
